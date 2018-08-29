c024afc1ac3ba22b12cd0b7f89ae68208efc9904
ag93999 <adityagupta93999@gmail.com>
2018-08-29 16:26:19 +0530

    Tutorial 5 finished.

--- pastebin/settings.py
+++ pastebin/settings.py
@@ -69,6 +69,13 @@ TEMPLATES = [
     },
 ]
 
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
+
+
 WSGI_APPLICATION = 'pastebin.wsgi.application'
 
 
